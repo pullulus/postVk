afeb3efb8cfd26b66ecfc4f6c673b4a0c5a38ca3
Elena Tsypileva <etsypileva@gmail.com>
2020-09-17 16:01:08 +0300

    added PostManager.java

---
+++ src/main/java/manager/PostManger.java
@@ -0,0 +1,15 @@
+package manager;
+
+public class PostManger {
+    private Post[] posts;
+
+    public void delete(int ownerId, int id) {
+        // TODO: add logic
+    }
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        // TODO: add logic
+
+        return null;
+    }
+}
--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -8,4 +8,4 @@ public class CommentsInfo {
     boolean canOpen;
 
     // + get/set на все поля
-}
+    }
